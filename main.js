d5ffd0b891b9673ffa5615525ace5d7b2786d10d
ruucm <ruucm.a@gmail.com>
2018-11-30 23:59:20 +0900

    refactor: make src directory

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './src/App'
 
-class Greeting extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-    }
-  }
-  handleChange(event) {
-    this.setState({ name: event.target.value })
-  }
-
-  componentDidMount() {
-    alert('componentDidMount')
-  }
-  render() {
-    return (
-      <div>
-        <p>{this.state.name}</p>
-        <span>Say Hello to World : </span>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleChange(e)}
-        />
-      </div>
-    )
-  }
-}
-ReactDOM.render(<Greeting />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
